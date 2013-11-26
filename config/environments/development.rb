03aaf8eb86c3d2fb6594789c32a15cbfbf67ec63
Berty Qiming Ruan <berty.ruan@gmail.com>
2013-11-26 01:36:52 -0500

    uploading to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,12 @@ NewApplication::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # if browser.chrome
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # end
+
+  #if browser.ie
+  #onfig.action_mailer.default_url_options = { :host => 'http://127.0.0.1:3000/ }
+  #end
 end
